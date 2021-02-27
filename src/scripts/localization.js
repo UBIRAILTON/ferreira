ae82894028071b47f2429792aa9d9de61eb61b09
Ubirailton ferreira <56268009+UBIRAILTON@users.noreply.github.com>
2021-02-27 11:04:57 -0300

    Delete src directory

--- src/scripts/localization.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-module.exports = async function search(cep){
-                    const localization = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-
-                    return localization.data
-                }
